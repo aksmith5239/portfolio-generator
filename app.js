2c4740b3603c332054bddf27fc08d3f3b1148c67
aksmith5239 <aklobby@gmail.com>
2020-09-23 14:50:30 -0600

    inquirer interface working

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const generatePage = require('./src/page-template.js');
-const fs = require('fs');
+const inquirer = require('inquirer');
+console.log(inquirer);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+.then(answers => console.log(answers));
+// const pageHTML = generatePage(name. github);
 
-const name = profileDataArgs[0];
-const github = profileDataArgs[1];
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-
-    console.log('Portfolio complete! Check out the index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out the index.html to see the output!');
+// });
